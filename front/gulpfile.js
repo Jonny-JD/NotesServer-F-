e1a638c0c888e23767b3a7d322be62e22b0fe801
JimmyD <cbrigante@mail.ru>
2025-01-05 21:57:16 +0300

    modified:   front/.gitignore 	modified:   front/gulpfile.js 	modified:   front/node_modules/.package-lock.json 	modified:   front/package-lock.json 	modified:   front/package.json 	modified:   front/src/index.html

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -1,17 +1,19 @@
 import gulp from "gulp";
 import autoprefixer from "gulp-autoprefixer";
 import {deleteAsync} from 'del';
+import browserSync from "browser-sync";
+import concat from "concat";
 
 var srcPaths = {
     css: "src/css/**/*.css",
     html: "src/*.html",
-    img: "src/img/*",
+    img: "src/img/*"
 }
 
 var buildPaths = {
     root: "build",
     css: "build/css",
-    img: "build/img",
+    img: "build/img"
 }
 
 function clearBuild() {
@@ -24,6 +26,7 @@ function css () {
         cascade: false
     }))
     .pipe(gulp.dest(buildPaths.css))
+    .pipe(browserSync.stream());
 }
 
 function img () {
@@ -34,13 +37,20 @@ function img () {
 function html () {
     return gulp.src(srcPaths.html)
     .pipe(gulp.dest(buildPaths.root))
+    .pipe(browserSync.stream());
 }
 
 
 let build = gulp.series(clearBuild, gulp.parallel(css, img, html));
 
 gulp.task("build", build);
+
 gulp.task("watch", gulp.series(build, function () {
-    gulp.watch(buildPaths.css, css);
-    gulp.watch(buildPaths.root, html);
+        browserSync.init({
+            server: {
+                baseDir: buildPaths.root
+            }
+    });
+    gulp.watch(srcPaths.css, css);
+    gulp.watch(srcPaths.html, html);
 }))
