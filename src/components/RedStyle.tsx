9e2f66d84da21261995e1ac037007087087a62ff
JimmyD <cbrigante@mail.ru>
2025-06-09 06:59:41 +0300

    Register page scroll 1.1

--- src/components/RedStyle.tsx
+++ src/components/RedStyle.tsx
@@ -6,6 +6,8 @@ import HomeHeader from "./header/HomeHeader.tsx";
 import mainGear from "@/assets/img/red/svg/main_gear.svg";
 import japanMain from "@/assets/img/red/svg/japan_main.svg";
 import redNumber from "@/assets/img/red/svg/side_number.svg";
+import ScrollContainer from "react-indiana-drag-scroll";
+import cn from "classnames";
 
 interface RedStyleProps {
     children: React.ReactNode;
@@ -18,21 +20,24 @@ const RedStyle: React.FC<RedStyleProps> = ({children, currentPage, totalPages}) 
         <div className={styles.container}>
             <img className={styles.sideNumber} src={sideNumber} alt="side-number"/>
             <HomeHeader currentPage={currentPage} totalPages={totalPages}/>
-            <main className={styles.main}>
-                <div className={styles.mainHeaderContainer}>
-                    <div className={styles.mainGearContainer}>
-                        <img className={styles.gearImage} src={mainGear} alt="Main Gear"/>
-                        <img className={styles.headerJapan} src={japanMain} alt="Japan Main"/>
-                    </div>
-                    <div className={styles.headerText}>
-                        <span>DIGITAL NOTES</span>
-                    </div>
-                </div>
+            <ScrollContainer className={cn(styles.main, styles.scrollConatiner)} hideScrollbars={true}>
                 <img className={styles.redNumber} src={redNumber} alt="Red Number"/>
+                <main className={styles.main}>
+                    <div className={styles.mainHeaderContainer}>
+                        <div className={styles.mainGearContainer}>
+                            <img className={styles.gearImage} src={mainGear} alt="Main Gear"/>
+                            <img className={styles.headerJapan} src={japanMain} alt="Japan Main"/>
+                        </div>
+                        <div className={styles.headerText}>
+                            <span>DIGITAL NOTES</span>
+                        </div>
+                    </div>
 
-                {children}
 
-            </main>
+                    {children}
+
+                </main>
+            </ScrollContainer>
             <footer className={styles.footer}>
                 <div className={styles.footerLine}>
                     <img className={styles.redLine} src={footerLine} alt="Footer Line"/>
