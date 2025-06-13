d3fd5978e509a56c6f30077ae8e9f18a25d6ddb3
JimmyD <cbrigante@mail.ru>
2025-06-13 19:28:29 +0300

    Loader fixes

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import cn from "classnames";
 import styles from "../styles/page/main_page.module.less";
 import RedStyle from "../components/RedStyle.tsx";
 import mainSignUpButton from "@/assets/img/red/svg/main_sign_up_button.svg";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Loader from "../components/Loader.tsx";
 
 const currentPage = 1;
@@ -14,48 +14,57 @@ const MainPage: React.FC = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const timeout = setTimeout(() => {
+        const handleLoad = () => {
             setLoading(false);
-        }, 1000);
+        };
 
-        return () => clearTimeout(timeout);
+        if (document.readyState === "complete") {
+            // если уже всё загружено
+            setLoading(false);
+        } else {
+            // иначе ждём полной загрузки
+            window.addEventListener("load", handleLoad);
+        }
+
+        return () => {
+            window.removeEventListener("load", handleLoad);
+        };
     }, []);
 
     const goToRegistration = () => {
         navigate("/registration");
     };
 
+    if (loading) {
+        return <Loader />;
+    }
 
     return (
-        <>
-            {loading && <Loader/>}
-            <RedStyle currentPage={currentPage} totalPages={totalPages}>
-                <div className={styles.contentBackgroundCover}>
-                    <div className={styles.mainText}>
-                        <p>
-                            In the neon haze of future megacities, digital notes are your
-                            cybernetic edge, pulsing with the speed of data streams.
-                            Harness the power of your mind with our next-gen note
-                            system—crafted for the fast-paced, tech-driven world. Capture,
-                            organize, and access your thoughts anytime, anywhere.
-                        </p>
-                    </div>
+        <RedStyle currentPage={currentPage} totalPages={totalPages}>
+            <div className={styles.contentBackgroundCover}>
+                <div className={styles.mainText}>
+                    <p>
+                        In the neon haze of future megacities, digital notes are your
+                        cybernetic edge, pulsing with the speed of data streams.
+                        Harness the power of your mind with our next-gen note
+                        system—crafted for the fast-paced, tech-driven world. Capture,
+                        organize, and access your thoughts anytime, anywhere.
+                    </p>
                 </div>
+            </div>
 
-                <div className={styles.mainButtonWrapper}>
-                    <div className={cn(styles.buttonWrapper, styles.loginButtonWrapper)}>
-                        <input
-                            type="image"
-                            className={cn(styles.headerButton, styles.cButton, styles.signUpMainB)}
-                            src={mainSignUpButton}
-                            alt="Main Sign Up Button"
-                            onClick={goToRegistration}
-                        />
-                    </div>
+            <div className={styles.mainButtonWrapper}>
+                <div className={cn(styles.buttonWrapper, styles.loginButtonWrapper)}>
+                    <input
+                        type="image"
+                        className={cn(styles.headerButton, styles.cButton, styles.signUpMainB)}
+                        src={mainSignUpButton}
+                        alt="Main Sign Up Button"
+                        onClick={goToRegistration}
+                    />
                 </div>
-
-            </RedStyle>
-        </>
+            </div>
+        </RedStyle>
     );
 };
 
