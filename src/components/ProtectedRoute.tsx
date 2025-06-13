87d621dd49f49259d765d2db52ac6631782cf116
JimmyD <cbrigante@mail.ru>
2025-06-13 19:13:14 +0300

    Merge branch 'View-fixes'

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext.tsx";
+import Loader from "./Loader.tsx";
 
 const ProtectedRoute: React.FC = () => {
     const { isLoggedIn, loading } = useAuth();
 
     if (loading) {
-        //TODO loader
-        return <div>Loading...</div>;
+        return <Loader/>;
     }
 
     if (!isLoggedIn) {
