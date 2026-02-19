55a8f6f028ddd84216e6d48e0d43eac84a111048
jimmy d <jonny.cbrigante@gmail.com>
2026-02-20 02:58:30 +0300

    register page design

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,9 +1,30 @@
 import type {JSX} from "react";
+import {Wrapper} from "../components/wrapper/Wrapper.tsx";
+import styles from "../styles/pages/RegisterPage.module.css";
 
 
 export const RegisterPage = (): JSX.Element => {
     return (
-        <>
-        </>
-    )
+        <Wrapper>
+            <div className={styles.mainContent}>
+                <form>
+                    <div className={"form-cell"}>
+                        <label htmlFor={"username"}>USERNAME</label>
+                        <input type={"text"} id={"username"} autoComplete={"username"}/>
+                    </div>
+                    <div className={"form-cell"}>
+                        <label htmlFor={"email"}>EMAIL</label>
+                        <input type={"email"} id={"email"} autoComplete={"email"}/>
+                    </div>
+                    <div className={"form-cell"}>
+                        <label htmlFor={"password"}>PASSWORD</label>
+                        <input type={"password"} id={"password"}/>
+                    </div>
+                    <button type={"submit"}>
+                        REGISTER
+                    </button>
+                </form>
+            </div>
+        </Wrapper>
+    );
 }
